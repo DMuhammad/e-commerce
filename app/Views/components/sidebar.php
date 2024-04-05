7dd04d2be511a31fd4eb84cf34927a11b9fbfb03
royhandf <royhandaffa14@gmail.com>
2024-04-06 05:45:57 +0700

    feat: dashboard components

---
+++ app/Views/components/sidebar.php
@@ -0,0 +1,65 @@
+<div id="sidebar">
+    <div class="sidebar-wrapper active">
+        <div class="sidebar-header position-relative">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="logo">
+                    <a href="index.html"><img src="./assets/compiled/svg/logo.svg" alt="Logo" srcset="" /></a>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar-menu">
+            <ul class="menu">
+                <li class="sidebar-title">Menu</li>
+
+                <li class="sidebar-item active">
+                    <a href="/dashboard" class="sidebar-link">
+                        <i class="bi bi-grid-fill"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item has-sub">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-stack"></i>
+                        <span>Master Data</span>
+                    </a>
+
+                    <ul class="submenu">
+                        <li class="submenu-item">
+                            <a href="/dashboard/company-profile" class="submenu-link">Company Profile</a>
+                        </li>
+                        <li class="submenu-item">
+                            <a href="/dashboard/categories" class="submenu-link">Categories</a>
+                        </li>
+                        <li class="submenu-item">
+                            <a href="/dashboard/categories" class="submenu-link">Products</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="sidebar-item">
+                    <a href="/dashboard/transactions" class="sidebar-link">
+                        <i class="bi bi-credit-card"></i>
+                        <span>Transactions</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a href="/dashboard/records" class="sidebar-link">
+                        <i class="bi bi-clipboard2-data-fill"></i>                        
+                        <span>Records</span>
+                    </a>
+                </li>
+
+                <hr>
+
+                <li class="sidebar-item">
+                    <a href="" class="sidebar-link">
+                        <i class="bi bi-box-arrow-in-left"></i>
+                        <span>Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
