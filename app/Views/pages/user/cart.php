b665b03a0c202f9674cf908653f888df71cc820b
royhandf <royhandaffa14@gmail.com>
2024-05-06 18:37:48 +0700

    fix: responsive cart view

--- app/Views/pages/user/cart.php
+++ app/Views/pages/user/cart.php
@@ -3,15 +3,15 @@
 <?= $this->section('content') ?>
 
 <div class="container my-5">
-    <h1 class="text-black fw-medium mb-3">Cart</h1>
-    <div class="row justify-content-between">
-        <div class="col-md-8">
-            <div class="d-flex gap-3 align-items-center mb-5">
-                <h6 class="fw-medium text-black mb-0">Cart</h6>
-                <hr class="text-black" style="width: 80px;">
-                <h6 class="fw-medium text-muted mb-0">Checkout</h6>
-                <hr class="text-black" style="width: 80px;">
-                <h6 class="fw-medium text-muted mb-0">Payment</h6>
+    <h1 class="text-black fw-medium mb-3 mx-md-0 mx-3">Cart</h1>
+    <div class="row justify-content-md-between justify-content-center">
+        <div class="col-md-8 col-11">
+            <div class="d-flex gap-1 align-items-center mb-5">
+                <a href="/cart" class="fw-medium text-black fs-6 mb-0">Cart</a>
+                <hr class="text-black" style="width: 50px;">
+                <a href="/checkout" class="fw-medium text-muted fs-6 mb-0">Checkout</a>
+                <hr class="text-black" style="width: 50px;">
+                <a href="/payment" class="fw-medium text-muted fs-6 mb-0">Payment</a>
             </div>
             <div class="d-flex flex-column mb-4">
                 <div class="row justify-content-between">
@@ -19,7 +19,7 @@
                         <img src="<?= base_url('assets/static/images/product.png') ?>" alt="product" height="200">
                     </div>
                     <div class="col-md-9 col-7">
-                        <h5>Hair Shampoo Treatment</h5>
+                        <h5 class="text-black">Hair Shampoo Treatment</h5>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                         <p class="text-black"><span class="text-muted">Variation: </span> 50 ml</p>
                         <h2 class="custom-color-primary">Rp. 50.000</h2>
@@ -27,7 +27,7 @@
                 </div>
                 <hr>
                 <div class="row justify-content-between">
-                    <div class="col-md-2 col-5">
+                    <div class="col-xl-3 col-md-4 col-5">
                         <div class="input-group number-spinner rounded-3" style="background-color: #f5f5f5;">
                             <span class="input-group-btn">
                                 <button class="btn" data-dir="dwn"><i class="fa-solid fa-minus"></i></button>
@@ -51,7 +51,7 @@
             </div>
         </div>
 
-        <div class="col-md-3">
+        <div class="col-xl-3 col-md-4 col-11">
             <div class="p-4 rounded-3" style="background-color: #f5f5f5;">
                 <h5 class="fw-medium mb-3">Order Summary</h5>
                 <div class="d-flex justify-content-between">
