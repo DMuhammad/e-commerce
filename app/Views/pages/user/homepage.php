5d09926ebbd2f06155a81612ddbc6af75ec287c3
royhandf <royhandaffa14@gmail.com>
2024-05-07 21:23:14 +0700

    refactor: remove carousel controls

--- app/Views/pages/user/homepage.php
+++ app/Views/pages/user/homepage.php
@@ -20,14 +20,6 @@
                 <img src="<?= base_url('assets/static/images/Capsule-Refill-Packaging-.3.jpg') ?>" class="d-block w-100" alt="product">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselIklan" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselIklan" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
     </div>
 </div>
 
